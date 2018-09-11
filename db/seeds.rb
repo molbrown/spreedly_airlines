b7886c320e6ae93765c0973bd28baac3fe4f8833
molbrown <molly@spreedly.com>
2018-09-11 15:52:55 -0400

    flight seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Flight.delete_all
+
+flights = Flight.create([
+    {destination: 'Honolulu, HI (HNL)', origin: 'Raleigh-Durham, NC (RDU)', price: 46400, tix_purchased: 0},
+    {destination: 'Paris, France (CDG)', origin: 'Raleigh-Durham, NC (RDU)', price: 124600, tix_purchased: 0},
+    {destination: 'London, UK (LHR)', origin: 'Raleigh-Durham, NC (RDU)', price: 118100, tix_purchased: 0},
+    {destination: 'Auckland, NZ (AKL)', origin: 'Raleigh-Durham, NC (RDU)', price: 155800, tix_purchased: 0},
+    {destination: 'Austin, TX (AUS)', origin: 'Raleigh-Durham, NC (RDU)', price: 33700, tix_purchased: 0},
+    {destination: 'Anchorage, AK (ANC)', origin: 'Raleigh-Durham, NC (RDU)', price: 60400, tix_purchased: 0}
+])
